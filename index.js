e063bd7308d1333b72b04d1d80395c2b888587b1
Vitor Michel <vitor.michel@valtech.com>
2021-06-04 15:11:07 -0300

    'route'

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 // const routes = require('../src/routes.js');
 const routes = require('./src/routes')
 var cors = require('cors');
+const { json } = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'OK'
+    })
+})
 
 app.use(cors());
 app.use(express.json());
