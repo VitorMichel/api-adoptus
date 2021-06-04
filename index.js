6d8c7f6157a37405e515b08b2c136f89d69b759c
Vitor Michel <vitor.michel@valtech.com>
2021-06-04 15:06:29 -0300

    'procfile'

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// const routes = require('../src/routes.js');
+const routes = require('./src/routes')
+var cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333, () => {
+    console.log('🚀 Server started!');
+})
