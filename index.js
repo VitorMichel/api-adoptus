68b85f3210c93a9794a226cb1b4444950a3a66ef
Vitor Michel <vitor.michel@valtech.com>
2021-06-11 09:32:15 -0300

    port

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const { json } = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        msg: 'OK'
-    })
-})
+let port = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => {
-    console.log('🚀 Server started!');
+app.listen(port, () => {
+    console.log('🚀 Server started on port: ' + port);
 })
